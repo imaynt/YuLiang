5ee72a7c2e110399eb99b02340ab40dd61e67c7b
lishisheng <541531745@qq.com>
2015-12-04 09:19:06 +0800

    啊

---
+++ yuliang/src/com/zykj/yuliang/activity/BindMobileActivity.java
@@ -0,0 +1,16 @@
+package com.zykj.yuliang.activity;
+
+import com.zykj.yuliang.BaseActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BindMobileActivity extends BaseActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+
+}
